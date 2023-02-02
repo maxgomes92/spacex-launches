a0d4dbd0d4ffa44175b1bf0966f6c1b9861d457d
Max Gomes <maxgomes92@gmail.com>
2023-02-02 20:16:37 +0100

    Add graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const FILMS_QUERY = gql`
+  query LaunchesQuery($limit: Int, $sort: String) {
+    launches(limit: $limit, sort: $sort) {
+      upcoming
+      launch_date_utc
+      id
+      mission_name
+    }
+  }
+`;
 
 function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY, { variables: { limit: 10 } }) as any
+
+  if (loading) return <>Loading...</>;
+  if (error) return <pre>{JSON.stringify(error.networkError.result.errors, null, 4)}</pre>
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>SpaceX Launches</h1>
+          <ul>
+            {data.launches.map((launch: any) => (
+              <li key={launch.id}>{launch.launch_date_utc} {launch.mission_name}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const client = new ApolloClient({
+  uri: "https://spacex-production.up.railway.app",
+  cache: new InMemoryCache(),
+});
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
