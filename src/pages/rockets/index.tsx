863c437f8b6bd441f8312dd115912f3a2f98e1e0
Max Gomes <maxgomes92@gmail.com>
2023-02-07 13:44:50 +0100

    ADd filter

--- src/pages/rockets/index.tsx
+++ src/pages/rockets/index.tsx
@@ -1,14 +1,23 @@
 import { Container } from "@mui/system";
-import { Button, Typography } from "@mui/material";
+import { Button, FormControl, FormControlLabel, Radio, RadioGroup, Typography } from "@mui/material";
 import { CardList, SpacerVertical } from '../../components'
 import { useRockets } from "../../hooks";
 import RocketItem from "../../components/RocketItem";
-import { useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 import { Rocket } from "../../generated/graphql";
 
 export default function RocketsPage() {
   const [sorted, setSorted] = useState<Rocket[]>()
+  const [countries, setCountries] = useState<string[]>([])
   const { data, loading, error } = useRockets()
+
+  useEffect(() => {
+    if (!data?.rockets) {
+      return;
+    }
+
+    setCountries(Array.from(new Set(data.rockets.map(rocket => rocket.country || ''))))
+  }, [data])
 
   if (error) {
     return (
@@ -45,6 +54,14 @@ export default function RocketsPage() {
     )
   }
 
+  const onCountryFilterChange = ({ target: { value }}: BaseSyntheticEvent) => {
+    if (!data?.rockets) return;
+
+    const filtered = data?.rockets.filter((rocket) => rocket.country === value)
+
+    setSorted(filtered)
+  }
+
   return (
     <Container>
       <SpacerVertical height={40} />
@@ -62,6 +79,25 @@ export default function RocketsPage() {
         <Button onClick={sortByDiameter}>Diameter</Button>
         <Button onClick={sortByWeight}>Weight</Button>
         <Button onClick={sortByCostPerLaunch}>Cost per launch</Button>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Typography variant="h6" component="h6">
+          Filter by country:
+        </Typography>
+
+        <FormControl style={{ paddingLeft: 10 }}>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
+            onChange={onCountryFilterChange}
+          >
+            {countries.map((country) => (
+              <FormControlLabel value={country} control={<Radio />} label={country} key={'country-radio_' + country} />
+            ))}
+          </RadioGroup>
+        </FormControl>
       </div>
 
       <SpacerVertical height={40} />
